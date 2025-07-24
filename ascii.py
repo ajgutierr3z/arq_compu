0afb664075145e9fc199c1427a339b4e711e6860
ajgutierrez <84606504+ajgutierr3z@users.noreply.github.com>
2025-07-24 14:54:33 -0600

    ascii
    codificación ascii

---
+++ ascii.py
@@ -0,0 +1,9 @@
+# Conversión de caracter a código ASCII
+caracter = 'A'
+codigo_ascii = ord(caracter)
+print(codigo_ascii)  # imprimira en pantalla: 65
+
+# Conversión de código ASCII a caracter
+codigo_ascii = 66
+caracter = chr(codigo_ascii)
+print(caracter)  # Imprimira en pantalla: B
